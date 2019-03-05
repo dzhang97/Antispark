81b40499c1d583f406431b9dcc2b1aff8a06a3d4
dzhang97 <deltazeta@berkeley.edu>
2019-03-04 19:07:52 -0800

    Enter power down mode while output is off
    Should reduce power consumption orders of magnitude lower than typical lithium self-discharge.
    
    still needs to be tested

--- antispark.c
+++ antispark.c
@@ -1,16 +1,25 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 int main(void) {
-	DDRB = 0xFF ^ (1<<PIN4);
-	PORTB = 0x00 | (1 << PORTB4);
+	DDRB = 0xFF ^ (1 << DDB2);
+	PORTB = 0x00;
 	OCR1A = 60;
 	TCCR1 = (1 << CTC1) | (1 << CS11) | (1 << CS12) | (1 << CS13);
 	uint8_t buttonDown = 0;
 	uint8_t on = 0;
 	uint8_t halfSeconds = 0;
+	
+	cli();			
+	GIMSK |= (1 << INT0);
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	MCUCR &= ~(1 << ISC00);
+	MCUCR |= (1 << ISC01);
+	sei();
 
 	for(;;) {
-		if (bit_is_clear(PINB, PB4)) {
+		if (bit_is_clear(PINB, PB2)) {
 			if (buttonDown == 0) {
 				TCCR1 = (1 << CTC1) | (1 << CS11) | (1 << CS12) | (1 << CS13);
 				buttonDown = 1;
@@ -21,7 +30,7 @@ int main(void) {
 					TCNT1 = 0;
 				}
 				if (halfSeconds == 2 && on == 0) {
-					PORTB = (1 << PORTB2) | (1 << PORTB3);
+					PORTB = (1 << PORTB1) | (1 << PORTB3);
 					on = 1;
 					halfSeconds = 0;
 					TCCR1 = 0x00;
@@ -32,12 +41,18 @@ int main(void) {
 					TCCR1 = 0x00;
 				}
 			}
-		} else if (bit_is_set(PINB, PB4)) {
+		} else if (bit_is_set(PINB, PB2)) {
 			buttonDown = 0;
 			halfSeconds = 0;
 			TCCR1 = 0x00;
+			
+			sleep_mode();
 		}
 	}
 	
 	return 0;
+}
+
+ISR(INT0_vect) {
+	//Check if needed
 }
