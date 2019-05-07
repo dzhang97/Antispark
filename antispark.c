43005e3e367e59b6a7ef4b2469466e896236558f
dzhang97 <deltazeta@berkeley.edu>
2019-05-07 16:21:06 -0700

    pcb revision 2

--- antispark.c
+++ antispark.c
@@ -1,6 +1,4 @@
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/sleep.h>
 
 int main(void) {
 	ADCSRA &= ~(1 << ADEN);
@@ -12,18 +10,15 @@ int main(void) {
 	uint8_t buttonDown = 0;
 	uint8_t on = 0;
 	uint8_t halfSeconds = 0;
-	
-	cli();			
-	GIMSK |= (1 << PCIE);
-	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-	PCMSK |= (1 << PCINT4);
-	sei();
 
 	for(;;) {
-		if (bit_is_clear(PINB, PB4)) {
+		if (bit_is_set(PINB, PB4)) {
 			if (buttonDown == 0) {
 				TCCR1 = (1 << CTC1) | (1 << CS11) | (1 << CS12) | (1 << CS13);
 				buttonDown = 1;
+				if (on == 1) {
+					PORTB = (1 << PORTB1);
+				}
 			} else {
 				if (TIFR & (1 << OCF1A)) {
 					halfSeconds += 1;
@@ -31,7 +26,7 @@ int main(void) {
 					TCNT1 = 0;
 				}
 				if (halfSeconds == 2 && on == 0) {
-					PORTB = (1 << PORTB2) | (1 << PORTB3);
+					PORTB = (1 << PORTB2);
 					on = 1;
 					halfSeconds = 0;
 					TCCR1 = 0x00;
@@ -42,18 +37,12 @@ int main(void) {
 					TCCR1 = 0x00;
 				}
 			}
-		} else if (bit_is_set(PINB, PB4)) {
+		} else if (bit_is_clear(PINB, PB4)) {
 			buttonDown = 0;
 			halfSeconds = 0;
 			TCCR1 = 0x00;
-			
-			sleep_mode();
 		}
 	}
 	
 	return 0;
-}
-
-ISR(PCINT0_vect) {
-	//Check if needed
 }
